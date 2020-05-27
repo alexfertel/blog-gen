6af1f8e738b5a9fa657b43d8ed1cc13a074ed576
Alexander Gonzalez <alexfertel97@gmail.com>
2020-05-27 17:09:41 -0400

    feat: Add icons to Home Page and change icons style

--- icons/index.js
+++ icons/index.js
@@ -1,8 +1,8 @@
-export { CSharpIcon } from './svgs/csharp.svg';
-export { CppIcon } from './svgs/cpp.svg';
-export { FSharpIcon } from './svgs/fsharp.svg';
-export { GolangIcon } from './svgs/golang.svg';
-export { PythonIcon } from './svgs/python.svg';
-export { RubyIcon } from './svgs/ruby.svg';
-export { JSIcon } from './svgs/javascript.svg';
-export { HaskellIcon } from './svgs/haskell.svg';
+export { default as CSharpIcon } from './svgs/csharp.svg';
+export { default as CppIcon } from './svgs/cpp.svg';
+export { default as FSharpIcon } from './svgs/fsharp.svg';
+export { default as GolangIcon } from './svgs/golang.svg';
+export { default as PythonIcon } from './svgs/python.svg';
+export { default as RubyIcon } from './svgs/ruby.svg';
+export { default as JSIcon } from './svgs/javascript.svg';
+export { default as HaskellIcon } from './svgs/haskell.svg';
