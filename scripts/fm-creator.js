0d4b230b2f7df19362de57314b99676cc377bf8a
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-05-27 12:57:54 -0400

    Apply suggestions from code review

---
+++ scripts/fm-creator.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+const postsFolder = '../_posts';
+
+const removeFrontmatter = fContent => {
+  const { content } = matter(fContent);
+  return content;
+};
+
+// const
+
+// Frontmatter Creator
+fs.readdir(postsFolder, (err, files) => {
+  if (err) throw err;
+
+  files.forEach(fName => {
+    if (fName === path.basename(__filename)) return;
+
+    fs.readFile(path.join(postsFolder, fName), 'utf-8', (error, data) => {
+      if (error) throw error;
+
+      // New frontmatter data
+      const frontmatter = {
+        title: 'Title goes here',
+        id: fName,
+        description: 'Description goes here',
+        url: 'URL goes here',
+      };
+
+      // Remove current frontmatter from data
+      const newData = removeFrontmatter(data);
+
+      const newFileContent = `---\n${Object.entries(frontmatter).reduce(
+        (acc, [k, v]) => `${acc}${k}: ${v}\n`,
+        ''
+      )}---\n${newData}`;
+      fs.writeFile(path.join(postsFolder, fName), newFileContent, wErr => {
+        if (wErr) throw wErr;
+      });
+    });
+  });
+});
