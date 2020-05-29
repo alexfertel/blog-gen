bf0f5fa2cdcb6f6ec2cd7f625cf463ffbea07ae2
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-05-29 15:30:51 -0400

    feat: add report head component

---
+++ components/report-head.js
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+export default function ReportHead({ title, rootClasses, titleClasses, textClasses }) {
+  return (
+    <div className={rootClasses}>
+      <h1 className={titleClasses}>{title}</h1>
+      <p className={textClasses}>
+        Los requerimientos de cada ejercicio del seminario serán expuestos desde el punto de vista práctico y teórico;
+        es decir, para su exposición, cada equipo se basará en el caso práctico en cuestión para introducir y explicar
+        el elemento teórico requerido. La exposición no es una mera enunciación de código. Preguntas como:{' '}
+        <em>¿Por qué?</em>, <em>¿Basándose en qué?</em>,{' '}
+        <em>
+          ¿Cómo se logra esto en el lenguaje <code>X</code>?
+        </em>{' '}
+        entre otras, deben h`acerse.
+      </p>
+      <p className={textClasses}>
+        Todos los miembros del equipo deben participar en la solución del ejercicio y estar preparados para exponer todo
+        el trabajo. <strong>La persona a exponer</strong> se decide el día de la exposición. Quién no esté presente en
+        la exposición de su equipo tiene <code>0</code> en la evaluación. (Note que estas notas se promedian y hay
+        distinción entre 0 y 2).
+      </p>
+    </div>
+  );
+}
