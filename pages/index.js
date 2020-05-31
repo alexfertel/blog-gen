3eb1b3a893dbab20df1e3bb15df1a71c94fd66c8
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-05-31 01:27:23 -0400

    feat: implement posts filtering

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { getIcon } from '../icons/utils';
 import { getAllPosts } from '../lib/api';
+
+const lcs = (stringPar, patternPar) => {
+  const string = stringPar.toLowerCase();
+  const pattern = patternPar.toLowerCase();
+
+  const m = string.length;
+  const n = pattern.length;
+
+  const dp = [];
+  let result = 0;
+  for (let i = 0; i < m + 1; i++) dp.push(Array(n + 1));
+
+  for (let i = 0; i < m + 1; i++) {
+    for (let j = 0; j < n + 1; j++) {
+      if (i === 0 || j === 0) dp[i][j] = 0;
+      else if (string[i - 1] === pattern[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        result = Math.max(result, dp[i][j]);
+      } else dp[i][j] = 0;
+    }
+  }
+
+  return result;
+};
+
+const useFilter = posts => {
+  const [filteredPosts, setFilteredPosts] = useState(posts);
+
+  const onKeywordsChange = e => {
+    const keywords = e.target.value;
+    setFilteredPosts(
+      posts
+        .map(post => {
+          const postData = [post.title, post.description, post.content].join(' ');
+          post.matchedLength = lcs(postData, keywords);
+          return post;
+        })
+        .sort((post1, post2) => (post1.matchedLength > post2.matchedLength ? '-1' : '1'))
+    );
+  };
+
+  return [filteredPosts, onKeywordsChange];
+};
 
 const ReportSummary = ({ report: { title, description, lang, url } }) => {
   const Icon = getIcon(lang);
@@ -20,55 +63,60 @@ const ReportSummary = ({ report: { title, description, lang, url } }) => {
   );
 };
 
-const Index = ({ posts }) => (
-  <div className="container max-w-6xl min-h-screen mx-auto">
-    <section className="text-gray-700 body-font">
-      <div className="container px-5 py-24 mx-auto">
-        <div className="flex flex-col flex-wrap items-center w-full text-center">
-          <h1 className="mb-2 text-2xl font-medium text-gray-900 sm:text-3xl title-font">
-            Seminarios de Lenguajes de Programación
-          </h1>
-          <p className="w-full text-base leading-relaxed lg:w-1/2">
-            {'Fuente en markdown '}
-            <a
-              href="https://github.com/alexfertel/blog-gen"
-              className="text-blue-500 focus:outline-none focus:text-blue-700 focus:underline hover:underline"
-            >
-              aquí.
-            </a>
-          </p>
-        </div>
-        <div className="flex justify-center mt-8">
-          <div className="relative flex items-center w-full h-12 max-w-3xl">
-            <div className="absolute inset-y-0 left-0 z-10 flex items-center ml-3">
-              <svg
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                className="w-5 h-5 text-gray-500"
+const Index = ({ posts }) => {
+  const [filteredPosts, onKeywordsChange] = useFilter(posts);
+
+  return (
+    <div className="container max-w-6xl min-h-screen mx-auto">
+      <section className="text-gray-700 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col flex-wrap items-center w-full text-center">
+            <h1 className="mb-2 text-2xl font-medium text-gray-900 sm:text-3xl title-font">
+              Seminarios de Lenguajes de Programación
+            </h1>
+            <p className="w-full text-base leading-relaxed lg:w-1/2">
+              {'Fuente en markdown '}
+              <a
+                href="https://github.com/alexfertel/blog-gen"
+                className="text-blue-500 focus:outline-none focus:text-blue-700 focus:underline hover:underline"
               >
-                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
+                aquí.
+              </a>
+            </p>
+          </div>
+          <div className="flex justify-center mt-8">
+            <div className="relative flex items-center w-full h-12 max-w-3xl">
+              <div className="absolute inset-y-0 left-0 z-10 flex items-center ml-3">
+                <svg
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5 text-gray-500"
+                >
+                  <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+              </div>
+              <input
+                className="absolute w-full py-3 pl-10 pr-4 font-medium text-gray-800 placeholder-gray-500 transition-all duration-300 bg-gray-200 border border-transparent rounded-lg shadow-sm focus:bg-white focus:border-gray-800 hover:border-gray-400 focus:outline-none"
+                placeholder="Intenta buscar seminarios (Título, contenido, etc.)"
+                type="text"
+                onChange={onKeywordsChange}
+              />
             </div>
-            <input
-              className="absolute w-full py-3 pl-10 pr-4 font-medium text-gray-800 placeholder-gray-500 transition-all duration-300 bg-gray-200 border border-transparent rounded-lg shadow-sm focus:bg-white focus:border-gray-800 hover:border-gray-400 focus:outline-none"
-              placeholder="Intenta buscar seminarios (Título, contenido, etc.)"
-              type="text"
-            />
+          </div>
+          <div className="flex flex-wrap mt-10 -m-4">
+            {filteredPosts.map(post => (
+              <ReportSummary key={post.url} report={post} />
+            ))}
           </div>
         </div>
-        <div className="flex flex-wrap mt-10 -m-4">
-          {posts.map(post => (
-            <ReportSummary key={post.url} report={post} />
-          ))}
-        </div>
-      </div>
-    </section>
-  </div>
-);
+      </section>
+    </div>
+  );
+};
 
 export async function getStaticProps() {
   const posts = getAllPosts();
