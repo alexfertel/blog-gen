635cb68578f34f999f3f159ecbe00c76b2ac46ff
Hieu Do Ngoc <hieudn.uh@gmail.com>
2020-06-02 15:02:33 -0400

    style: tone down breadcrumb colors

--- components/BreadcrumbNav.js
+++ components/BreadcrumbNav.js
@@ -3,24 +3,24 @@ import Link from 'next/link';
 import { ChevronRightIcon, HomeIcon } from '../icons/index';
 
 const BreadcrumbNav = ({ postTitle }) => (
-  <nav className="text-xs sm:text-sm font-bold text-gray-500 bg-gray-200 rounded-r-full" aria-label="Breadcrumb">
+  <nav className="text-xs sm:text-sm font-bold text-gray-400 bg-gray-200 rounded-r-full" aria-label="Breadcrumb">
     <ol className="list-none p-1 inline-flex">
       <li className="flex items-center">
-        <HomeIcon className="text-gray-700 px-1 h-3" />
+        <HomeIcon className="px-1 h-3" />
         <Link href="/">
-          <span className="transition-colors duration-150 hover:text-gray-800">Home</span>
+          <span className="transition-colors duration-150 hover:text-gray-700">Home</span>
         </Link>
-        <ChevronRightIcon className="text-gray-800 h-3 sm:mx-1" />
+        <ChevronRightIcon className="text-gray-600 h-3 sm:mx-1" />
       </li>
       <li className="flex items-center">
         {/*TODO: link to author's blogs*/}
         <Link href="#">
-          <span className="transition-colors duration-150 hover:text-gray-800">Author</span>
+          <span className="transition-colors duration-150 hover:text-gray-700">Author</span>
         </Link>
-        <ChevronRightIcon className="text-gray-800 h-3 sm:mx-1" />
+        <ChevronRightIcon className="text-gray-600 h-3 sm:mx-1" />
       </li>
       <li>
-        <span className="text-black">{postTitle}</span>
+        <span className="text-gray-500">{postTitle}</span>
       </li>
     </ol>
   </nav>
