43dd9ee93641c1adea1257c2f0239d05b8114932
Alexander Gonzalez <alexfertel97@gmail.com>
2020-06-01 23:51:50 -0400

    style: There can only be one transition-X class applied

--- components/PostNavigation.js
+++ components/PostNavigation.js
@@ -3,27 +3,27 @@ import Link from 'next/link';
 import { LeftArrowIcon, RightArrowIcon } from '../icons/index';
 
 const PostNavigation = ({ nextSlug, prevSlug }) => (
-  <div className="flex w-full justify-between pt-12 pb-10">
-    <div className="justify-start">
-      {prevSlug && (
-        <Link href={`/posts/${prevSlug}`}>
-          <button type="button" className="btn">
-            <LeftArrowIcon className="w-4 h-4 mr-1" />
-            Anterior
-          </button>
-        </Link>
-      )}
-    </div>
-    <div className="justify-end">
-      {nextSlug && (
-        <Link href={`/posts/${nextSlug}`}>
-          <button type="button" className="btn">
-            Siguiente
-            <RightArrowIcon className="w-4 h-4 ml-1" />
-          </button>
-        </Link>
-      )}
-    </div>
+  <div className="flex justify-between w-full pt-12 pb-10">
+    {prevSlug ? (
+      <Link href={`/posts/${prevSlug}`}>
+        <button type="button" className="btn">
+          <LeftArrowIcon className="w-4 h-4 mr-1" />
+          Anterior
+        </button>
+      </Link>
+    ) : (
+      <span />
+    )}
+    {nextSlug ? (
+      <Link href={`/posts/${nextSlug}`}>
+        <button type="button" className="btn">
+          Siguiente
+          <RightArrowIcon className="w-4 h-4 ml-1" />
+        </button>
+      </Link>
+    ) : (
+      <span />
+    )}
   </div>
 );
 
