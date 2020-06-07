73caea40f481ef88949335e4b9fe39023e814fab
Alexander Gonzalez <alexfertel97@gmail.com>
2020-06-07 10:15:19 -0400

    style: Update sketch and form styles

--- components/Fields.js
+++ components/Fields.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PasswordInput from './PasswordInput';
 
 const inputStyles =
-  'w-full px-4 py-2 mt-4 text-base leading-6 transition-all duration-300 bg-gray-200 border border-gray-200 placeholder-gray-500 rounded focus:bg-white focus:outline-none hover:border-blue-300 focus:border-blue-300';
+  'w-full px-4 py-2 mt-4 text-base leading-6 transition-all duration-300 bg-gray-200 border border-gray-200 placeholder-gray-600 font-medium rounded focus:bg-white focus:outline-none hover:border-blue-300 focus:border-blue-300';
 
 export const LoginFields = ({ register, errors }) => (
   <>
--- components/PasswordInput.js
+++ components/PasswordInput.js
@@ -10,7 +10,7 @@ const PasswordInput = ({ registeredRef, placeholder, name, error }) => {
       <div className="relative flex items-center w-full h-12 max-w-3xl">
         <input
           ref={registeredRef}
-          className="absolute w-full py-2 pl-4 pr-10 leading-6 text-gray-800 placeholder-gray-500 transition-all duration-300 bg-gray-200 border border-gray-200 rounded focus:bg-white focus:border-blue-300 hover:border-blue-300 focus:outline-none"
+          className="absolute w-full py-2 pl-4 pr-10 font-medium leading-6 text-gray-800 placeholder-gray-600 transition-all duration-300 bg-gray-200 border border-gray-200 rounded focus:bg-white focus:border-blue-300 hover:border-blue-300 focus:outline-none"
           placeholder={placeholder}
           name={name}
           autoComplete="off"
