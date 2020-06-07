1bb0ea69da065073d175ed39cfef03880b7d012a
Alexander Gonzalez <alexfertel97@gmail.com>
2020-06-06 22:26:08 -0400

    feat: Add p5 and AuthBackground component

--- hocs/withAnimation.js
+++ hocs/withAnimation.js
@@ -2,16 +2,14 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-
 const withAnimation = WrappedComponent => props => (
   <motion.div
-    initial={{ x: 600, opacity: 0 }}
+    initial={{ x: 300, opacity: 0 }}
     animate={{ x: 0, opacity: 1 }}
-    transition={{ delay: 0.3, type: 'tween' }}
-    exit={{ opacity: 0 }}
+    transition={{ delay: 0.1, type: 'tween', opacity: { delay: 0.4 }, duration: 0.7 }}
   >
     <WrappedComponent {...props} />
   </motion.div>
 );
 
-export default withAnimation
+export default withAnimation;
