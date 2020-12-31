d1e0a3a9c7390c826fb70e6b42a2db58703eae71
Alexander Gonzalez <alexfertel97@gmail.com>
2020-12-31 12:38:32 -0500

    Remove auth completely

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,11 @@ import 'typeface-quicksand';
 import '../styles/index.css';
 import '../styles/highlight.css';
 import Layout from '../components/shared/Layout';
-import UserProvider from '../hooks/useUser';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </UserProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
