9e93d3c7dca304f6485d8b3570e01f7e440ee8b9
Alexander Gonzalez <alexfertel97@gmail.com>
2020-12-31 13:30:11 -0500

    Revamp solution

--- components/reports/ReportNavigation.js
+++ components/reports/ReportNavigation.js
@@ -6,7 +6,7 @@ const ReportNavigation = ({ nextSlug, prevSlug }) => (
   <div className="flex justify-between w-full pt-12 pb-10">
     {prevSlug ? (
       <Link href={`/posts/${prevSlug}`}>
-        <button type="button" className="btn">
+        <button type="button" className="inline-flex items-center px-3 py-1 text-sm transition-all duration-500 transform bg-gray-200 border-0 rounded hover:scale-105 hover:bg-gray-300">
           <LeftArrowIcon className="w-4 h-4 mr-1" />
           Anterior
         </button>
@@ -16,7 +16,7 @@ const ReportNavigation = ({ nextSlug, prevSlug }) => (
     )}
     {nextSlug ? (
       <Link href={`/posts/${nextSlug}`}>
-        <button type="button" className="btn">
+        <button type="button" className="inline-flex items-center px-3 py-1 text-sm transition-all duration-500 transform bg-gray-200 border-0 rounded hover:scale-105 hover:bg-gray-300">
           Siguiente
           <RightArrowIcon className="w-4 h-4 ml-1" />
         </button>
